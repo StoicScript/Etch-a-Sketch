3ed101f426f230f763a4fb242d9a0e3e8c9e506e
Alexander <alexanderiaconetti@gmail.com>
2021-12-16 14:15:22 -0500

    added rainbox functionality

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const clear = document.getElementById('clear-btn')
 const range = document.getElementById('range')
 const rangeNum = document.getElementById("range-num").innerHTML = `${range.value} x ${range.value}`
 const colorSelection = document.getElementById('color-picker')
+const rainbox = document.getElementById('rainbow')
 
 let columns = 16
 let cells = []
 let row 
 let cell
 let cellDimensions
+
+// Table Creation
 
 function createTable(){
     for (let i = 0; i < columns; i++){
@@ -39,25 +42,47 @@ function createCell(){
     return cell
 };
 
+// Cell Color
+
 function cellHoverColor(){
     cells.forEach(cell => {
         cell.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = colorSelection.value
+            if(rainbox.checked){
+                e.target.style.backgroundColor = getRandomColor()
+            } else {
+                e.target.style.backgroundColor = colorSelection.value
+            }
         })
     })
 };
 
-function resetColor(){
-    cells.forEach(cell => cell.style.backgroundColor = 'white');
-}
+// Update Container Size 
 
 function resetContainer(){
     while (container.firstChild) {
         container.firstChild.remove()
     }
     cells = [];
-    askForCells();
+    gridSizeSelection();
 }
+
+function gridSizeSelection(){
+    columns = range.value
+};
+
+// Update Grid Size Text 
+
+range.addEventListener('input', (e) => {
+    gridSizeText(e.target.value)
+})
+
+function gridSizeText(val) {
+    document.getElementById("range-num").innerHTML = `${val} x ${val}`;
+}
+
+// Update Grid Size 
+
+range.addEventListener('change', newSize)
 
 function newSize(){
     resetColor();
@@ -65,21 +90,23 @@ function newSize(){
     createTable();
 }
 
-function askForCells(){
-    columns = range.value
-};
-
-function gridSizeText(val) {
-    document.getElementById("range-num").innerHTML = `${val} x ${val}`;
-}
+// Reset Cell Color
 
 clear.addEventListener('click', () => {
     resetColor();
 })
 
-range.addEventListener('input', (e) => {
-    gridSizeText(e.target.value)
-})
+function resetColor(){
+    cells.forEach(cell => cell.style.backgroundColor = 'white');
+}
 
-range.addEventListener('change', newSize)
+// Random Color Generator
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
